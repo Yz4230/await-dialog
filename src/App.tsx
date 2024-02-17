e05981506fda8485d4a43a722fdfc5c7d1a19e0d
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2024-02-18 08:54:25 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="container mx-auto">
+      <header className="bg-gray-100 px-4 py-2">
+        <h1 className="text-2xl">Await Dialog Example</h1>
+      </header>
+    </main>
+  );
+}
